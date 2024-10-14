f7bfd20ff12323168c23685139d64d2259526796
Pablo Alcolea Sesse <128633352+PabloAlcoleaSesse@users.noreply.github.com>
2024-10-14 09:58:10 +0200

    Creacion CMakeLists.txt / Resultado del ejercicio 1 y 2

---
+++ Proyecto 1/Main.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Pablo Alcolea Sesse on 14/10/24.
+//
+
+#include <iostream>
+
+int suma(int a, int b){
+    return a+b;
+}
+int g = 20;
+int main() {
+    std::cout << "Hola Mundo" << std::endl;
+    std::cout << "La suma es: " << suma(5, 3) << std::endl;
+    std::cout << "Valor de g antes de la función suma: " << g << std::endl;
+    std::cout << "La suma es: " << suma(5, g) << std::endl;
+    std::cout << "Valor de g después de la función suma: " << g << std::endl;
+    return 0;
+}
+
---
+++ Proyecto 1/Main.h
@@ -0,0 +1,16 @@
+//
+// Created by Pablo Alcolea Sesse on 14/10/24.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+class Main {
+
+};
+
+
+
+#endif //MAIN_H
