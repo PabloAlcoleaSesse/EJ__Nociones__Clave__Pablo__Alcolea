490f2515d64c6241330e972758dff12f5b2d0899
Pablo Alcolea Sesse <128633352+PabloAlcoleaSesse@users.noreply.github.com>
2024-10-15 15:40:39 +0200

    Creacion de la clase persona en Main.cpp y de la clase estudiante en la función Main.cpp

--- Proyecto_1/Main.cpp
+++ Proyecto_1/Main.cpp
@@ -3,6 +3,8 @@
 //
 
 #include <iostream>
+#include <Proyecto_1/Persona.h>
+#include <Proyecto_1/Estudiante.h>
 
 int suma(int a, int b){
     return a+b;
@@ -14,6 +16,21 @@ int main() {
     std::cout << "Valor de g antes de la función suma: " << g << std::endl;
     std::cout << "La suma es: " << suma(5, g) << std::endl;
     std::cout << "Valor de g después de la función suma: " << g << std::endl;
+
+    Persona persona_1;
+    persona_1.setNombre("Pablo") ;
+    persona_1.setEdad(18);
+    persona_1.setGenero("Masculino");
+
+    Estudiante estudiante_1;
+    estudiante_1.setNombre("Pablo");
+    estudiante_1.setEdad(18);
+    estudiante_1.setGenero("Masculino");
+    estudiante_1.setGrado("Ingieneria Informatica") ;
+    estudiante_1.mostrarDetalles();
+
+
+
     return 0;
 }
 
